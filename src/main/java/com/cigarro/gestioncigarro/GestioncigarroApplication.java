4c51eef29418984efa80036d634a588627798bc4
Ignacio Lobos <ialobossep@gmail.com>
2026-05-11 18:02:47 -0400

    Se agrega microservicio Producto

---
+++ src/main/java/com/cigarro/gestioncigarro/GestioncigarroApplication.java
@@ -0,0 +1,13 @@
+package com.cigarro.gestioncigarro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioncigarroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioncigarroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cigarro/gestioncigarro/GestioncigarroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cigarro.gestioncigarro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioncigarroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
